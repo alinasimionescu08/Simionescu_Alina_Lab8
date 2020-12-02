930241de16fb619e4c4097fe5c0a461781053b5a
Alina Simionescu <alinasimionescu505@yahoo.com>
2020-12-02 15:26:56 +0200

    Simionescu_Alina_Lab9

---
+++ Simionescu_Alina_Lab8/Models/ErrorMessageAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Simionescu_Alina_Lab8.Models
+{
+    internal class ErrorMessageAttribute : Attribute
+    {
+    }
+}
---
+++ Simionescu_Alina_Lab8/Models/Simionescu_Alina_Lab9Context.cs
@@ -0,0 +1,6 @@
+﻿namespace Simionescu_Alina_Lab8.Models
+{
+    public class Simionescu_Alina_Lab9Context
+    {
+    }
+}
